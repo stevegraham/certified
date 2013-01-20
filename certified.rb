8e2e5063f954464f7e3832c5dc404c85706bda04
Mark Burns <mark@shutl.com>
2013-01-20 17:35:58 +0000

    Use a magic number for extra security :lol:

--- certified.rb
+++ certified.rb
@@ -5,7 +5,27 @@ Net::HTTP.class_eval do
 
   def use_ssl= boolean
     self.ca_file     = "#{File.dirname(__FILE__)}/certs/ca-bundle.crt"
-    self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    
+    # Possibly the only good use case for a magic number
+    #
+    # Some tutorials and very naughty gems suggest doing:
+    #   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    #
+    # irb> OpenSSL::SSL::VERIFY_PEER
+    # => 1
+    # irb> OpenSSL::SSL::VERIFY_NONE
+    # => 0
+    # irb> OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    # (irb) warning: already initialized constant VERIFY_PEER
+    # => 0
+    #  
+    # Thus invalidating a line like
+    #
+    #   self.verify_mode = OpenSSL::SSL::VERIFY_PEER 
+    #
+    # and requiring hardcoding the value `1'
+    #
+    self.verify_mode = 1
     self._use_ssl    = boolean
   end
 end
